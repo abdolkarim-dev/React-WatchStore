4f5e0a3dc4ebc680349fc7acfc3bb472ad4946cc
abdolkarim-dev <bazdarabdolkarim@gmail.com>
2026-09-08 10:54:05 +0330

    Update: add about and watches page for route

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,9 +1,12 @@
 import Home from "../pages/Home";
 import Collections from "../pages/Collections";
-import Layout from "../components/layout/Layout";
+import About from "../pages/About";
+import Watches from "../pages/Watches";
 const routes = [
   { path: "/", element: <Home /> },
   { path: "/collections", element: <Collections /> },
+  { path: "/about", element: <About /> },
+  { path: "/watches", element: <Watches /> },
 ];
 
 export default routes;
