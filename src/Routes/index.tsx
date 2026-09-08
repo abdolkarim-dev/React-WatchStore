968e57afca6967d4d45fc0cbdeeae9603d7eb226
abdolkarim-dev <bazdarabdolkarim@gmail.com>
2026-09-08 10:28:06 +0330

    feat: add router for site

---
+++ src/Routes/index.tsx
@@ -0,0 +1,8 @@
+import Home from "../pages/Home";
+import Collections from "../pages/Collections";
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/collections", element: <Collections /> },
+];
+
+export default routes;
